19250c89e7879763cad59050d2cd39e1301f8cb9
vladqew <121374205+vladqew@users.noreply.github.com>
2023-01-29 08:21:04 +0200

    add conter.js

---
+++ counter.js
@@ -0,0 +1,13 @@
+let counter = 0;
+
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    if (counter % 10 === 0 ) {
+        alert(`Count now is ${counter}` )
+    }
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').addEventListener('click', count)
+})
